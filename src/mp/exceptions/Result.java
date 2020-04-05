59e8c7d2445364b4107c9a1727bb119871ba562b
Israel <israelsonseca@gmail.com>
2020-04-05 16:03:02 +0200

    Register FUNCTION is correctly developed

---
+++ src/mp/exceptions/Result.java
@@ -0,0 +1,4 @@
+package mp.exceptions;
+
+public class Result {
+}
---
+++ src/mp/exceptions/UsuarioRegistrado.java
@@ -0,0 +1,8 @@
+package mp.exceptions;
+
+import mp.users.MiembroURJC;
+
+public class UsuarioRegistrado extends Throwable {
+    public UsuarioRegistrado(MiembroURJC nuevoUsuario) {
+    }
+}
