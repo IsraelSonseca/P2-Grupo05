b4e2635d606e16f202391a7292690c0fe13a3f97
PabloAtahoneroGB <p.atahonero.2017@alumnos.urjc.es>
2020-04-14 17:07:36 +0200

    Prueba Commit Changed

--- src/mp/subforos/Comentario.java
+++ src/mp/subforos/Comentario.java
@@ -1,18 +1,29 @@
 package mp.subforos;
 
+import java.util.LinkedList;
+
 public class Comentario extends ObjetoPuntuable {
-
-	Comentario esRespondido;
+        
+	// Comentario esRespondido;
+        private LinkedList<Comentario> respuestas;
+        private String texto;
 	private TipoComentario tipo;
-	private int id;
-
-	public Comentario(int puntos) {
+	private int id = 0;
+        
+	public Comentario(int puntos,String  texto,TipoComentario tipo) {
 		super(puntos);
+                this.texto=texto;
+                this.tipo=tipo;
+                id=id+1;
+                this.id=id;
 	}
+        
 
 	public void comentar() {
-		// TODO - implement Comentario.comentar
-		throw new UnsupportedOperationException();
+            id=id++;
+
+            Comentario comment=new Comentario();		// TODO - implement Comentario.comentar
+		
 	}
 
 }
