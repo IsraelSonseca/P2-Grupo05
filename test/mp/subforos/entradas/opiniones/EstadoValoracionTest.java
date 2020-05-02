923b4e08f99e92b2741880c3da0d626e85b0231f
Israel <israelsonseca@gmail.com>
2020-05-02 14:04:15 +0200

    EstadoValoracionTest

--- test/mp/subforos/entradas/opiniones/EstadoValoracionTest.java
+++ test/mp/subforos/entradas/opiniones/EstadoValoracionTest.java
@@ -1,5 +1,6 @@
 package mp.subforos.entradas.opiniones;
 
+import mp.subforos.entradas.EstadoEntrada;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,20 @@ public class EstadoValoracionTest {
 
     @Test
     public void valuesTest() {
+        EstadoValoracion[] valores = EstadoValoracion.values();
+        assertTrue(valores.length==2);
+        EstadoValoracion[] valoresEsperados = new EstadoValoracion[2];
+        valoresEsperados[0]=EstadoValoracion.positiva;
+        valoresEsperados[1]=EstadoValoracion.negativa;
+        assertArrayEquals(valoresEsperados,valores);
     }
 
     @Test
     public void valueOfTest() {
+        EstadoValoracion creada = EstadoValoracion.valueOf("positiva");
+        assertEquals(creada,EstadoValoracion.positiva);
+        EstadoValoracion validada = EstadoValoracion.valueOf("negativa");
+        assertEquals(validada,EstadoValoracion.negativa);
     }
+
 }
